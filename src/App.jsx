b8b1af5a9a2884cd97a0060b6b5c293fbf281b93
rsnr-code <rishadnero@gmail.com>
2022-12-21 15:30:26 -0500

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from './constants/routes'
+import useAuthListener from "./hooks/use-auth-listener";
+import UserContext from './context/user'
 import './styles/app.css'
 
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"))
+const Dashboard = lazy(() => import("./pages/Dashboard"))
+const NotFound = lazy(() => import("./pages/NotFound"))
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
+    <UserContext.Provider value={{user}}>
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          <Route path='*' element={<NotFound />} />
         </Routes> 
       </Suspense>
     </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
-import * as ROUTES from '../constants/routes';
-
+import * as ROUTES from "../constants/routes";
+import { doesUsernameExist } from '../services/firebase';
 
 const Signup = () => {
-  const history = useNavigate();
+  const navigate = useNavigate();
   const { firebase } = useContext(FirebaseContext);
 
   const [username, setUsername] = useState("");
@@ -20,10 +20,36 @@ const Signup = () => {
   const handleSignup = async (event) => {
     event.preventDefault();
 
-    try {
-       
-    } catch (error) {
-       
+    const usernameExists = await doesUsernameExist(username);
+    if (!usernameExists.length) {
+      try {
+        const createdUserResult = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+
+        await createdUserResult.user.updateProfile({
+          displayName: username
+        });
+
+        await firebase.firestore().collection('users').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLowerCase(),
+          fullName,
+          email: email.toLowerCase(),
+          following: [], //if you want users to automatically follow, stick them in here (2:33:06)
+          dateCreated: Date.now()
+        })
+
+        navigate(ROUTES.DASHBOARD);
+      } catch (error) {
+        setFullName('');
+        setEmail('');
+        setFullName('');
+        setPassword('');
+        setError(error.message)
+      }
+    } else {
+      setError('That username is already taken, please try another.')
     }
   };
 
@@ -41,56 +67,69 @@ const Signup = () => {
       </div>
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-        </h1>
-     
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
 
-        <form onSubmit={handleSignup} method="POST">
-          <input
-            aria-label="Enter your username"
-            type="text"
-            placeholder="Username"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setUsername(target.value)}
-            value={username}
-          />
-          <input
-            aria-label="Enter your full name"
-            type="text"
-            placeholder="Full Name"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setFullName(target.value)}
-            value={fullName}
-          />
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email Address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setEmail(target.value)}
-            value={email}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setPassword(target.value)}
-            value={password}
-          />
-          <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}>Log In</button>
-        </form>
-      </div>
-      <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
-        <p className="text-sm">Have an account?{` `}
-        <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
-            Log In
-        </Link>
-        </p>
-      </div>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleSignup} method="POST">
+            <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+              value={username}
+            />
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full Name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email Address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmail(target.value)}
+              value={email}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Sign Up
+            </button>
+          </form>
         </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Have an account?{` `}
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Log In
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
